c8ebbd936481ea264d77df0db0bd1cde5c6d1a91
Dazk <phungxuandoan2001@gmail.com>
2023-09-19 10:39:38 +0700

    Update part 2

--- Acme.BookStore/src/Acme.BookStore.Web/Menus/BookStoreMenuContributor.cs
+++ Acme.BookStore/src/Acme.BookStore.Web/Menus/BookStoreMenuContributor.cs
@@ -33,6 +33,20 @@ public class BookStoreMenuContributor : IMenuContributor
                 order: 0
             )
         );
+        context.Menu.AddItem(
+            new ApplicationMenuItem(
+            "BooksStore",
+            l["Menu:BookStore"],
+            icon: "fa fa-book"
+            ).AddItem(
+            new ApplicationMenuItem(
+                "BooksStore.Books",
+                l["Menu:Books"],
+                url: "/Books"
+                    )
+                )
+        );
+
 
         if (MultiTenancyConsts.IsEnabled)
         {
